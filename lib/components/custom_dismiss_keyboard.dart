c5f64c5dda277533b60c563ab0724f1316ff98da
Leandre <leajapo@gmail.com>
2021-03-15 20:23:32 +0200

    feat: added new reusable components

---
+++ lib/components/custom_dismiss_keyboard.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomDismissKeyboard extends StatelessWidget {
+  final Widget child;
+
+  const CustomDismissKeyboard({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanDown: (_) {
+        FocusScope.of(context).unfocus();
+      },
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: child,
+    );
+  }
+}
---
+++ lib/components/custom_loading_widget.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomLoadingWidget extends StatelessWidget {
+  const CustomLoadingWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isAndroid = Platform.isAndroid;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          isAndroid
+              ? CircularProgressIndicator()
+              : CupertinoActivityIndicator(),
+          SizedBox(height: 20),
+          Text(
+            'Loading please wait',
+            style: TextStyle(fontSize: 15),
+          ),
+        ],
+      ),
+    );
+  }
+}

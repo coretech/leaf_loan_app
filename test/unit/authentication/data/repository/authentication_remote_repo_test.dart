a737591fe369da82b49e4f75a5392e0682030903
hellyab <yabsra23@gmail.com>
2022-02-23 15:08:29 +0300

    test: add wrong credentails test

--- test/unit/authentication/data/repository/authentication_remote_repo_test.dart
+++ test/unit/authentication/data/repository/authentication_remote_repo_test.dart
@@ -6,6 +6,15 @@ import 'package:loan_app/core/abstractions/abstractions.dart';
 import 'package:loan_app/core/ioc/ioc.dart';
 import 'package:mocktail/mocktail.dart';
 
+const _token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+    '.eyJpZCI6IjVlZmUwM2U3YWE3MGFhNWVlNDQ0OGFl'
+    'ZSIsImZuYW1lIjoiTGVhZiIsImxuYW1lIjoiVGVzdG'
+    'luZyIsInVzZXJuYW1lIjoiY3VzdG9tZXIiLCJyb2xlI'
+    'jpbInVzZXIiLCJjdXN0b21lciJdLCJzdGF0dXMiOiJhY'
+    '3RpdmUiLCJpYXQiOjE2MzYxMDA2OTIsImV4cCI6MTYzN'
+    'jExNTA5Mn0.r6fG2XjPIc00ybf4CazhT2aXRg7S-7Tw62'
+    'y73bxNnDw';
+
 void main() {
   setUpAll(() async {
     registerFallbackValue(StackTrace.empty);
@@ -31,24 +40,41 @@ void main() {
         ),
       ),
     ).thenAnswer((invocation) async {
-      return HttpResponse(statusCode: 200, data: {
-        'status': true,
-        'message': 'User logged in successfully',
-        'type': 'Bearer',
-        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-            '.eyJpZCI6IjVlZmUwM2U3YWE3MGFhNWVlNDQ0OGFl'
-            'ZSIsImZuYW1lIjoiTGVhZiIsImxuYW1lIjoiVGVzdG'
-            'luZyIsInVzZXJuYW1lIjoiY3VzdG9tZXIiLCJyb2xlI'
-            'jpbInVzZXIiLCJjdXN0b21lciJdLCJzdGF0dXMiOiJhY'
-            '3RpdmUiLCJpYXQiOjE2MzYxMDA2OTIsImV4cCI6MTYzN'
-            'jExNTA5Mn0.r6fG2XjPIc00ybf4CazhT2aXRg7S-7Tw62'
-            'y73bxNnDw'
-      });
+      return HttpResponse(
+        statusCode: 200,
+        data: {
+          'status': true,
+          'message': 'User logged in successfully',
+          'type': 'Bearer',
+          'token': _token,
+        },
+      );
+    });
+    when(
+      () => IntegrationIOC.httpHelper().post(
+        data: {
+          'username': 'username',
+          'password': 'wrong password',
+        },
+        url: any(
+          named: 'url',
+          that: contains('/userservice/signin'),
+        ),
+      ),
+    ).thenAnswer((invocation) async {
+      return HttpResponse(
+        statusCode: 200,
+        data: {
+          'code': 401,
+          'status': false,
+          'message': 'Username or Password is incorrect'
+        },
+      );
     });
   });
 
   test(
-    'Given user credentials, '
+    'Given correct user credentials, '
     'When AuthRemoteRepo.login is called with the credentials, '
     'Then an instance of Right<AuthenticationResult> with '
     'token and etc should be returned',
@@ -62,11 +88,32 @@ void main() {
       response.fold(
         (l) {},
         (r) {
-          expect(r.token.isNotEmpty, true);
+          expect(r.token, _token);
           expect(r.status, true);
           expect(r.message, 'User logged in successfully');
           expect(r.type, 'Bearer');
         },
+      );
+    },
+  );
+
+  test(
+    'Given wrong  user credentials, '
+    'When AuthRemoteRepo.login is called with the credentials, '
+    'Then an instance of Left with error message',
+    () async {
+      final authRemoteRepo = AuthRemoteRepo();
+      final response = await authRemoteRepo.login(
+        username: 'username',
+        password: 'wrong password',
+      );
+      expect(response, isA<Left>());
+      response.fold(
+        (l) {
+          expect(l, isA<AuthFailure>());
+          expect(l.reason, Reason.serverError);
+        },
+        (r) {},
       );
     },
   );

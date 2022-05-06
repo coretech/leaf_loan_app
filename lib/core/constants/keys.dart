8637ef7d86679ef1e4e944b9eca153fbb03bba4f
Yabsra <yabsra.abdurahman@idt.net>
2022-05-06 19:20:11 +0300

    feat: move smartlook config to remoteconfig

--- lib/core/constants/keys.dart
+++ lib/core/constants/keys.dart
@@ -1,9 +1,9 @@
 class Keys {
+  static String deviceToken = 'deviceToken';
   static String firstName = 'firstName';
   static String language = 'language';
   static String onboardingStatus = 'onboardingStatus';
   static String token = 'token';
   static String userId = 'userId';
-  static String deviceToken = 'deviceToken';
   static String username = 'username';
 }

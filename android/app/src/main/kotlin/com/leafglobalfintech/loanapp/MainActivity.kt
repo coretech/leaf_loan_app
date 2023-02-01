618a3eda9e945fa0d1dcc9f032c1029476d8f82f
Yabsra Abdurahman <101590404+idt-this-is-yab@users.noreply.github.com>
2023-02-01 15:41:56 +0300

    Feature - credo flutter sdk (#86)
    * fix: credo sdk
    
    * fix: ios sdk
    
    * docs: update CHANGELOG and version number
    
    * fix: linting issues
    
    * fix: update linter flutter version
    
    ---------
    
    Co-authored-by: Yabsra <yabsra23@gmail.com>

--- android/app/src/main/kotlin/com/leafglobalfintech/loanapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/leafglobalfintech/loanapp/MainActivity.kt
@@ -1,83 +1,13 @@
 package com.leafglobalfintech.loanapp
 
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.*
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-import android.annotation.SuppressLint
-import android.os.Build
-
-import credoapp.CredoAppResult
-import credoapp.CredoAppService
-import credoapp.module.account.RegisterAccountModule
-import credoapp.module.calendar.CalendarModule
-import credoapp.module.contact.ContactModule
-import credoapp.module.iovation.FraudForceModule
-import credoapp.module.media.MediaModule
 import java.util.*
 
 class MainActivity : FlutterActivity() {
-    lateinit var credoAppService: CredoAppService
-
-    private val CHANNEL = "com.leafglobal.loanapp/credolabs"
-
-    // The scope for the UI thread
-    private val mainScope = CoroutineScope(Dispatchers.Main)
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "submitCredoLabsData") {
-
-                val authKey: String = call.argument("authKey")!!;
-                val referenceNumber: String = call.argument("referenceNumber")!!;
-                val url: String = call.argument("url")!!;
-                mainScope.launch {
-                    withContext(Dispatchers.IO) {
-                        try {
-                            val storedReferenceNumber = submitCredoLabsData(authKey, referenceNumber, url)
-                            runOnUiThread {
-                                result.success(storedReferenceNumber!!)
-                            }
-                        } catch (e: Exception) {
-                            runOnUiThread {
-                                result.error("Error", e.message, e)
-                            }
-                        }
-                    }
-                }
-            } else {
-                result.notImplemented()
-            }
-        }
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
-
-    suspend fun submitCredoLabsData(authKey: String, referenceNumber: String, url: String): String? {
-        var storedReferenceNumber: String?
-        val context = applicationContext
-        // create an instance of CredoAppService
-        if (::credoAppService.isInitialized.not())
-            credoAppService = CredoAppService.Builder(context, "${url}/", authKey)
-                    .addModule(CalendarModule())
-                    .addModule(ContactModule())
-                    .addModule(MediaModule())
-                    .addModule(RegisterAccountModule())
-                    .addModule(FraudForceModule())
-                    .build()
-
-        // Collects and uploads data from phone to the CredoLab web-service
-        storedReferenceNumber = when (val result = credoAppService.execute(referenceNumber)) {
-            is CredoAppResult.Success -> result.value
-            is CredoAppResult.Error -> throw Exception("${result.message} ${result.code}")
-        }
-        return storedReferenceNumber
-    }
-
 }
